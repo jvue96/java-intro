2a5b3922c4d70a3446671e2bd0553dbaa2e5bf2e
Juno Vue <Juno.Vue@Hotmail.com>
2019-05-07 16:18:33 -0500

    string Concatenation works for helloName method

--- hello.java
+++ hello.java
@@ -1,4 +1,4 @@
-/* hello world application */
+/* hello world application practice */
 
 
 public class hello {
@@ -7,11 +7,25 @@ public class hello {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello World!");
         addNumbers(); 
+        helloName();
     }
 
+    // Prints sum of integers 
     static void addNumbers() {
         int a = 5;
         int b = 5;
+        int c = 10; 
+        int d = 10; 
         System.out.println("The sum of a and b is = " + Integer.sum(a, b));
+        System.out.println("The sum of a and b is = " + Integer.sum(c, d));
     }
+
+    static void helloName() {
+        String Juno = "Juno"; 
+        String Vue = "Vue";
+        System.out.println(Juno + " " + Vue);
+    }
+
+
+
 }
